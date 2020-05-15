a04f518d86ce24496a7452ee7bb44792efe9d0cb
Maxim <maksim.torba@nixsolutions.com>
2020-05-15 13:02:09 +0300

    Created lib Battleship.Services.Interfaces with interface file

---
+++ Battleship.Services.Interfaces/IGameService.cs
@@ -0,0 +1,22 @@
+﻿using Battleship.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleship.Services.Interfaces
+{
+    public interface IGameService
+    {
+        void CreateGame(int userId);
+
+        void CreateField(int gameId, int playerId);
+
+        void MakeMove(Step step, int gameId, int playerId);
+
+        void JoinGame(int gameId, int userId);
+
+        void JoinGame(int gameId, string username);
+
+        void JoinGame(int gameId, ApplicationUser user);
+    }
+}
