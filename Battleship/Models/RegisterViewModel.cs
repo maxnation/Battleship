11d27b64e1374ae9e3363a83e353cc1a07183f0b
Maxim <maksim.torba@nixsolutions.com>
2020-05-19 12:50:44 +0300

    Added Login and Register View Models

---
+++ Battleship/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Battleship.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        [DataType(DataType.Password)]
+        [Required]
+        public string ConfirmPassword { get; set; }
+    }
+}
