31aad861e4c0dcdeb4a5e6ffac2f17f1ea8c3df0
Maxim <maksim.torba@nixsolutions.com>
2020-05-22 11:32:14 +0300

    Added GameHub. Now it adds players to group by gameId

---
+++ Battleship/GameHub.cs
@@ -0,0 +1,26 @@
+﻿using Battleship.Domain.Interfaces;
+using Battleship.Services.Interfaces;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    public class GameHub : Hub
+    {
+        private readonly IGameService gameService;
+        private readonly IUnitOfWork unitOfWork;
+
+        public GameHub(IGameService gameService, IUnitOfWork unitOfWork)
+        {
+            this.gameService = gameService;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task JoinGame(int gameId, string username)
+        {
+            string groupName = gameId.ToString();
+            await this.Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await this.Clients.Group(groupName).SendAsync("joinGameLog", username, gameId);
+        }
+    }
+}
