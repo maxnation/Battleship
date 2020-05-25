26665aa3b26bdb9d8d6d39777d0e07874cca33bf
Maxim <maksim.torba@nixsolutions.com>
2020-05-25 12:12:47 +0300

    Added method which maps game data on game statistics view model

--- Battleship/Mapping/GameMap.cs
+++ Battleship/Mapping/GameMap.cs
@@ -10,6 +10,43 @@ namespace Battleship.Mapping
 {
     public static class GameMap
     {
+        public static IEnumerable<GameStatisticsViewModel> GetGameStatisticsViewModel(IEnumerable<Game> games)
+        {
+            List<GameStatisticsViewModel> gameVMs = new List<GameStatisticsViewModel>();
+
+            foreach (var game in games)
+            {
+                Player winner = game.Players.First(p => p.IsWinner);
+                var winnerShips = winner.Field.Cells
+                    .Where(c => c.State == CellState.Hit || c.State == CellState.Occupied)
+                    .Select(c => c.Ship)
+                    .Distinct(new ShipsEqualityComparer())
+                    .ToList();
+
+                GameStatisticsViewModel gameStatisticsVM = new GameStatisticsViewModel
+                {
+                    GameNo = game.Id,
+
+                    WinnerUsername = winner.User.Email,
+
+                    LoserUsername = game.Players.First(p => !p.IsWinner).User.Email,
+
+                    NumberOfMovesInGame = (byte)game.Players.SelectMany(p => p.Steps).Count(),
+
+                    WinnerShips = winnerShips
+                    .Select(sh => new ShipStatisticsViewModel { Size = sh.Size, XP = sh.XP })
+                    .ToArray()
+                };
+
+                gameStatisticsVM.TotalShipsSurvived = (byte)gameStatisticsVM.WinnerShips.Where(s => s.XP != 0).Count();
+                gameStatisticsVM.TotalXP = (byte)gameStatisticsVM.WinnerShips.Select(s => (int)s.XP).Sum();
+
+                gameVMs.Add(gameStatisticsVM);
+            }
+
+            return gameVMs;
+        }
+
         public static GameViewModel GetGameViewModel(IRepository<Game> repository, string username, int gameId)
         {
             Game game = repository.GetQueryable()
