f0e7b1284dce5246ce1f7e8fcdd71f97e77e67e8
Maxim <maksim.torba@nixsolutions.com>
2020-05-25 12:05:15 +0300

    Added View Models for statistics

---
+++ Battleship/Models/GameStatisticsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Battleship.Models
+{
+    public class GameStatisticsViewModel
+    {
+        public int GameNo { get; set; }
+
+        public string WinnerUsername { get; set; }
+
+        public string LoserUsername { get; set; }
+
+        public byte TotalXP { get; set; }
+
+        public byte TotalShipsSurvived { get; set; }
+
+        public byte NumberOfMovesInGame { get; set; }
+
+        public ShipStatisticsViewModel[] WinnerShips { get; set; }
+    }
+}
