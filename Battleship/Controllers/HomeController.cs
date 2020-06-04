40c607a6456d7909a3ad9824dbe6d69d590dcf1a
Maxim <maksim.torba@nixsolutions.com>
2020-06-04 17:26:24 +0300

    Cleaned up existing code

--- Battleship/Controllers/HomeController.cs
+++ Battleship/Controllers/HomeController.cs
@@ -1,29 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Battleship.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Battleship.Models;
+using System.Diagnostics;
 
 namespace Battleship.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
