abf78aaa3bd2f69e9a22e019553aa65ffc2c19c5
Maxim <maksim.torba@nixsolutions.com>
2020-06-04 17:41:17 +0300

    Added js method which handles tumblr switching

---
+++ Battleship/wwwroot/js/statistics.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    let allGamesList = document.getElementById("allGamesContainer");
+    let userOnlyGamesList = document.getElementById("userOnlyGamesContainer");
+    userOnlyGamesList.hidden = true;
+    $('.switch-btn').click(function (e, changeState) {
+        if (changeState === undefined) {
+            $(this).toggleClass('switch-on');
+        }
+        if ($(this).hasClass('switch-on')) {
+            $(this).trigger('on.switch');
+        } else {
+            $(this).trigger('off.switch');
+        }
+    });
+
+    $('.switch-btn').on('on.switch', function () {
+        allGamesList.hidden = true;
+        userOnlyGamesList.hidden = false;
+    });
+
+    $('.switch-btn').on('off.switch', function () {
+        allGamesList.hidden = false;
+        userOnlyGamesList.hidden = true;
+    });
+
+    $('.switch-btn').each(function () {
+        $(this).triggerHandler('click', false);
+    });
+});
